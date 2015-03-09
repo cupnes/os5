75cf45b95eb8c448d1473ce154e43abb3c34d22d
Yuma Arakawa <yuarkw@gmail.com>
2015-03-09 20:20:58 +0900

    メッセージ出力を関数化
    また、AH=0x13 -> AH=0x0e へ方針変更

--- boot.s
+++ boot.s
@@ -2,23 +2,33 @@
 
 	cli
 
+	movw	$0x07c0, %ax
+	movw	%ax, %ds
+	movw	$0x0000, %ax
+	movw	%ax, %ss
+	movw	$0x1000, %sp
+
 	/* ビデオモード設定(画面クリア) */
 	movw	$0x0000, %ax
 	int		$0x10
 
-	/* 文字列出力 */
-	BOOTSEG = 0x07c0
-	movw	$0x0000, %dx
-	movw	$5, %cx
-	movw	$0x0007, %bx
-	movw	$BOOTSEG, %ax
-	movw	%ax, %es
-	movw	$msg, %bp
-	movw	$0x1301, %ax
-	int		$0x10
+	movw	$msg, %si
+	call	print_msg
 
 end:
 	jmp		end
 
+print_msg:
+	lodsb
+	andb	%al, %al
+	jz		print_msg_ret
+	movb	$0xe, %ah
+	movw	$7, %bx
+	int		$0x10
+	jmp		print_msg
+print_msg_ret:
+	ret
+
 msg:
 	.ascii	"Hello"
+	.byte	0
