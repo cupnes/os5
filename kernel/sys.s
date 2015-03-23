8f88efc763f855b7b06d407232bccd8e6e5dc58d
Yuma Arakawa <yuarkw@gmail.com>
2015-03-23 21:34:16 +0900

    本番用GDTをプロテクトモードで読み込み
    併せて、本番用GDTを追加

--- kernel/sys.s
+++ kernel/sys.s
@@ -6,6 +6,8 @@
 
 	movl	$0x00800000, %esp
 
+	lgdt gdt_descr
+
 	pushl $0
 	pushl $0
 	pushl $0
@@ -17,4 +19,12 @@ end:
 	jmp		end
 
 	.data
+gdt_descr:
+	.word 3*8-1
+	.long gdt
+gdt:
+	.quad 0x0000000000000000	/* NULL descriptor */
+	.quad 0x00cf9a000000ffff	/* 4GB(r-x:Code) */
+	.quad 0x00cf92000000ffff	/* 4GB(rw-:Data) */
+
 	.bss
