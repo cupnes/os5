677d894d95b0144292061cecfc7bb8b68649f5fc
Yuma Arakawa <yuarkw@gmail.com>
2015-08-15 19:41:33 +0900

    コメントを修正

--- kernel/main.c
+++ kernel/main.c
@@ -65,10 +65,11 @@ int main(void)
 	gdt[3].type = 9;
 	gdt[3].p = 1;
 
+	/* Setup exception handler */
 	for (i = 0; i < EXCEPTION_NUM; i++)
 		intr_set_handler(i, (unsigned int)&exception_handler);
 
-	/* Setup shell_tss */
+	/* Setup Task Register */
 	load_task_register();
 	put_str("task loaded.\r\n");
 
@@ -96,6 +97,7 @@ int main(void)
 	task1_tss.fs = 0x0010;
 	task1_tss.gs = 0x0010;
 
+	/* Setup interrupt handler and mask register */
 	con_init();
 	intr_set_handler(32, (unsigned int)&timer_handler);
 	intr_set_handler(INTR_NUM_KB, (unsigned int)&keyboard_handler);
@@ -105,12 +107,14 @@ int main(void)
 	intr_set_mask_master(mask);
 	sti();
 
+	/* Setup PIT */
 	outb_p(0x34, 0x0043);
 	/* 割り込み周期11932(0x2e9c)サイクル(=100Hz、10ms毎)に設定 */
 	outb_p(0x9c, 0x0040);
 	outb_p(0x2e, 0x0040);
 	put_str("Timer initialized.\r\n");
 
+	/* Start main task */
 	start_shell();
 
 	return 0;
