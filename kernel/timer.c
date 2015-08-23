8da6d439400d418d8cd80d12e31621e392cf8ed1
Yuma Arakawa <yuarkw@gmail.com>
2015-08-23 16:03:47 +0900

    タイマー設定を定数化

--- kernel/timer.c
+++ kernel/timer.c
@@ -21,7 +21,9 @@ void do_ir_timer(void)
 void timer_init(void)
 {
 	/* Setup PIT */
-	outb_p(0x34, IOADR_PIT_CONTROL_WORD);
+	outb_p(IOADR_PIT_CONTROL_WORD_BIT_COUNTER0
+	       | IOADR_PIT_CONTROL_WORD_BIT_16BIT_READ_LOAD
+	       | IOADR_PIT_CONTROL_WORD_BIT_MODE2, IOADR_PIT_CONTROL_WORD);
 	/* 割り込み周期11932(0x2e9c)サイクル(=100Hz、10ms毎)に設定 */
 	outb_p(0x9c, IOADR_PIT_COUNTER0);
 	outb_p(0x2e, IOADR_PIT_COUNTER0);
