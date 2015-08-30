33762a53c3d7a7f544d8caf2f9aa98fa8cfcc742
Yuma Arakawa <yuarkw@gmail.com>
2015-08-30 13:21:28 +0900

    スケジューラをタスク非依存に修正

--- kernel/sched.c
+++ kernel/sched.c
@@ -2,20 +2,12 @@
 #include <io_port.h>
 #include <intr.h>
 #include <timer.h>
-#include <shell.h>
-#include <uptime.h>
 
 struct task task[TASK_NUM];
 
 void schedule(void)
 {
-	if (global_counter % 2) {
-		outb_p(IOADR_MPIC_OCW2_BIT_MANUAL_EOI | INTR_IR_TIMER,
-		       IOADR_MPIC_OCW2);
-		task[UPTIME_ID].context_switch();
-	} else {
-		outb_p(IOADR_MPIC_OCW2_BIT_MANUAL_EOI | INTR_IR_TIMER,
-		       IOADR_MPIC_OCW2);
-		task[SHELL_ID].context_switch();
-	}
+	outb_p(IOADR_MPIC_OCW2_BIT_MANUAL_EOI | INTR_IR_TIMER,
+	       IOADR_MPIC_OCW2);
+	task[global_counter % TASK_NUM].context_switch();
 }
