2a9bb33d73d416ea753f15d6e5f8dbe1dca5f66a
Yuma Arakawa <yuarkw@gmail.com>
2015-09-13 02:44:06 +0900

    mm: PDE・PTE初期化処理追加

--- kernel/main.c
+++ kernel/main.c
@@ -9,7 +9,29 @@
 int main(void)
 {
 	unsigned char mask;
-	unsigned char i;
+	unsigned int i;
+
+	struct page_directory_entry {
+		union {
+			struct {
+				unsigned int all;
+			};
+			struct {
+				unsigned int p: 1, r_w: 1, u_s: 1, pwt: 1, pcd: 1, a: 1, reserved: 1, ps: 1, g: 1, usable: 3, pt_base: 20;
+			};
+		};
+	} *pde;
+	struct page_table_entry {
+		union {
+			struct {
+				unsigned int all;
+			};
+			struct {
+				unsigned int p: 1, r_w: 1, u_s: 1, pwt: 1, pcd: 1, a: 1, d: 1, pat: 1, g: 1, usable: 3, page_base: 20;
+			};
+		};
+	} *pte;
+	unsigned int paging_base_addr;
 
 	/* Setup console */
 	cli();
@@ -24,6 +46,40 @@ int main(void)
 	/* Setup devices */
 	con_init();
 	timer_init();
+	/* mmu_init(); */
+	pde = (struct page_directory_entry *)0x00090000;
+	pde->all = 0;
+	pde->p = 1;
+	pde->r_w = 1;
+	pde->pwt = 1;
+	pde->pcd = 1;
+	pde->pt_base = 0x00091;
+	pde++;
+	for (i = 0; i < 1023; i++) {
+		pde->all = 0;
+		pde++;
+	}
+
+	pte = (struct page_table_entry *)0x00091000;
+	for (i = 0; i < 0x007; i++) {
+		pte->all = 0;
+		pte++;
+	}
+	paging_base_addr = 0x00007e00;
+	for (; i <= 0x080; i++) {
+		pte->all = 0;
+		pte->p = 1;
+		pte->r_w = 1;
+		pte->pwt = 1;
+		pte->pcd = 1;
+		pte->page_base = paging_base_addr;
+		paging_base_addr += 0x00001000;
+		pte++;
+	}
+	for (; i < 0x1000; i++) {
+		pte->all = 0;
+		pte++;
+	}
 
 	/* Setup tasks */
 	shell_init();
