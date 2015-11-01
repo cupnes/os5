0004e73e2f0b8fa6ae1a0d525dc7a54f643dd7aa
Yuma Arakawa <yuarkw@gmail.com>
2015-11-01 15:30:31 +0900

    lock: ロック機能追加
    これまではsti()/cli()のみで、入れ子にできなかった。
    
    そこで、専用のロック関数kern_lock()/kern_unlock()で、
    「ロック区間の始めで既に割り込み無効の場合はunlock時に何もせず抜ける」よう、
    入れ子に対応した。
    
    なお、現状で提供しているロック機能はグローバルロックのみ。

---
+++ include/kernel.h
@@ -0,0 +1,7 @@
+#ifndef _KERNEL_H_
+#define _KERNEL_H_
+
+void kern_lock(unsigned char *if_bit);
+void kern_unlock(unsigned char *if_bit);
+
+#endif /* _KERNEL_H_ */
--- kernel/main.c
+++ kernel/main.c
@@ -4,9 +4,27 @@
 #include <memory.h>
 #include <console_io.h>
 #include <timer.h>
+#include <kernel.h>
 #include <kern_task.h>
 #include <shell.h>
 #include <uptime.h>
+
+void kern_lock(unsigned char *if_bit)
+{
+	/* Save EFlags.IF */
+	*if_bit = (x86_get_eflags() & X86_EFLAGS_IF) ? 1 : 0;
+
+	/* if saved IF == true, then cli */
+	if (*if_bit)
+		cli();
+}
+
+void kern_unlock(unsigned char *if_bit)
+{
+	/* if saved IF == true, then sti */
+	if (*if_bit)
+		sti();
+}
 
 int main(void)
 {
