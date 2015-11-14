e1ef8c8540f1428b741b36eed225b021a62e733b
Yuma Arakawa <yuarkw@gmail.com>
2015-11-14 16:55:26 +0900

    syscall: システムコール番号はenumで定義するよう修正

--- include/kernel.h
+++ include/kernel.h
@@ -1,8 +1,10 @@
 #ifndef _KERNEL_H_
 #define _KERNEL_H_
 
-#define SYSCALL_TIMER_GET_GLOBAL_COUNTER	1
-#define SYSCALL_SCHED_WAKEUP_MSEC		2
+enum {
+	SYSCALL_TIMER_GET_GLOBAL_COUNTER = 1,
+	SYSCALL_SCHED_WAKEUP_MSEC
+};
 
 void kern_lock(unsigned char *if_bit);
 void kern_unlock(unsigned char *if_bit);
