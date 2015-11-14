37a6f37fc61163441ac999dc9817ab73d1e006f8
Yuma Arakawa <yuarkw@gmail.com>
2015-11-14 17:07:55 +0900

    syscall: con: 座標指定の文字列出力システムコール追加

--- apps/uptime.c
+++ apps/uptime.c
@@ -15,10 +15,10 @@ void uptime_start(void)
 		uptime = syscall(SYSCALL_TIMER_GET_GLOBAL_COUNTER, 0, 0, 0) / 100;
 		cursor_pos_y = syscall(SYSCALL_CON_GET_CURSOR_POS_Y, 0, 0, 0);
 		if (cursor_pos_y < ROWS) {
-			put_str_pos("uptime:", COLUMNS - (7 + 4), 0);
+			syscall(SYSCALL_CON_PUT_STR_POS, (unsigned int)"uptime:", COLUMNS - (7 + 4), 0);
 			dump_hex_pos(uptime, 4, COLUMNS - 4, 0);
 		} else {
-			put_str_pos("uptime:", COLUMNS - (7 + 4), cursor_pos_y - ROWS + 1);
+			syscall(SYSCALL_CON_PUT_STR_POS, (unsigned int)"uptime:", COLUMNS - (7 + 4), cursor_pos_y - ROWS + 1);
 			dump_hex_pos(uptime, 4, COLUMNS - 4, cursor_pos_y - ROWS + 1);
 		}
 		syscall(SYSCALL_SCHED_WAKEUP_MSEC, 33, 0, 0);
