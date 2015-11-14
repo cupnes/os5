cd2539d6a2687488467a6de5cfc9e90fbdd65b8f
Yuma Arakawa <yuarkw@gmail.com>
2015-11-14 18:08:40 +0900

    syscall: システムコール定義箇所のswitch-caseの構造を修正

--- kernel/main.c
+++ kernel/main.c
@@ -31,31 +31,38 @@ void kern_unlock(unsigned char *if_bit)
 
 unsigned int do_syscall(unsigned int syscall_id, unsigned int arg1, unsigned int arg2, unsigned int arg3)
 {
+	unsigned int result = -1;
+
 	switch (syscall_id) {
 	case SYSCALL_TIMER_GET_GLOBAL_COUNTER:
-		return timer_get_global_counter();
+		result = timer_get_global_counter();
 		break;
 	case SYSCALL_SCHED_WAKEUP_MSEC:
 		wakeup_after_msec(arg1);
-		return 0;
+		result = 0;
+		break;
 	case SYSCALL_CON_GET_CURSOR_POS_Y:
-		return (unsigned int)cursor_pos.y;
+		result = (unsigned int)cursor_pos.y;
+		break;
 	case SYSCALL_CON_PUT_STR:
 		put_str((char *)arg1);
-		return 0;
+		result = 0;
+		break;
 	case SYSCALL_CON_PUT_STR_POS:
 		put_str_pos((char *)arg1, (unsigned char)arg2, (unsigned char)arg3);
-		return 0;
+		result = 0;
+		break;
 	case SYSCALL_CON_DUMP_HEX:
 		dump_hex(arg1, arg2);
-		return 0;
+		result = 0;
 		break;
 	case SYSCALL_CON_DUMP_HEX_POS:
 		dump_hex_pos(arg1, arg2, (unsigned char)(arg3 >> 16), (unsigned char)(arg3 & 0x0000ffff));
-		return 0;
+		result= 0;
+		break;
 	}
 
-	return -1;
+	return result;
 }
 
 int main(void)
