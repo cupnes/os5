a2b358598d8f18d2a56aaf71db7f7a163aec1029
Yuma Arakawa <yuarkw@gmail.com>
2015-12-21 20:07:23 +0900

    mem: PDE/PTEの構造体宣言をヘッダーファイルへ移動

--- kernel/include/memory.h
+++ kernel/include/memory.h
@@ -1,6 +1,27 @@
 #ifndef __MEMORY_H__
 #define __MEMORY_H__
 
+struct page_directory_entry {
+	union {
+		struct {
+			unsigned int all;
+		};
+		struct {
+			unsigned int p: 1, r_w: 1, u_s: 1, pwt: 1, pcd: 1, a: 1, reserved: 1, ps: 1, g: 1, usable: 3, pt_base: 20;
+		};
+	};
+};
+struct page_table_entry {
+	union {
+		struct {
+			unsigned int all;
+		};
+		struct {
+			unsigned int p: 1, r_w: 1, u_s: 1, pwt: 1, pcd: 1, a: 1, d: 1, pat: 1, g: 1, usable: 3, page_base: 20;
+		};
+	};
+};
+
 void mem_init(void);
 void mem_page_start(void);
 
--- kernel/memory.c
+++ kernel/memory.c
@@ -4,26 +4,8 @@
 
 void mem_init(void)
 {
-	struct page_directory_entry {
-		union {
-			struct {
-				unsigned int all;
-			};
-			struct {
-				unsigned int p: 1, r_w: 1, u_s: 1, pwt: 1, pcd: 1, a: 1, reserved: 1, ps: 1, g: 1, usable: 3, pt_base: 20;
-			};
-		};
-	} *pde;
-	struct page_table_entry {
-		union {
-			struct {
-				unsigned int all;
-			};
-			struct {
-				unsigned int p: 1, r_w: 1, u_s: 1, pwt: 1, pcd: 1, a: 1, d: 1, pat: 1, g: 1, usable: 3, page_base: 20;
-			};
-		};
-	} *pte;
+	struct page_directory_entry *pde;
+	struct page_table_entry *pte;
 	unsigned int paging_base_addr;
 	unsigned int i;
 	unsigned int cr4;
