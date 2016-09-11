de307277272d87d0ab3ed1a4eea3c7ff86cb1eb2
Yuma Ohgami <yuma@ohgami.jp>
2016-09-11 18:09:51 +0900

    app: whoareyou: support main() as entry point
    whoareyouアプリケーションのエントリーポイントとして、main関数を実行する
    よう修正する。
    
    main関数をtextセクションの先頭に配置するため、whoareyou.cではmain関数
    に__attribute__でentryセクションへの配置を指定し、リンカスクリプトでは
    entryセクションをtextセクションの先頭に配置するよう修正している。

--- apps/whoareyou/whoareyou.c
+++ apps/whoareyou/whoareyou.c
@@ -1,12 +1,6 @@
 #include <kernel.h>
 
-unsigned int syscall(unsigned int syscall_id, unsigned int arg1, unsigned int arg2, unsigned int arg3);
-
-void entry(void)
-{
-	syscall(SYSCALL_CON_PUT_STR, (unsigned int)"OS5\r\n", 0, 0);
-	syscall(SYSCALL_EXIT, 0, 0, 0);
-}
+int main(void) __attribute__((section(".entry")));
 
 unsigned int syscall(unsigned int syscall_id, unsigned int arg1, unsigned int arg2, unsigned int arg3)
 {
@@ -18,4 +12,12 @@ unsigned int syscall(unsigned int syscall_id, unsigned int arg1, unsigned int ar
 	:"a"(syscall_id), "b"(arg1), "c"(arg2), "d"(arg3));
 
 	return result;
+}
+
+int main(void)
+{
+	syscall(SYSCALL_CON_PUT_STR, (unsigned int)"OS5\r\n", 0, 0);
+	syscall(SYSCALL_EXIT, 0, 0, 0);
+
+	return 0;
 }
