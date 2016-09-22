acda1e4724fb5e76cf56a2ee642db19f6cfe55ff
Yuma Ohgami <yuma@ohgami.jp>
2016-09-22 15:24:58 +0900

    kernel: task: replace magic number with APP_STACK_BASE_UESR macro
    タスク(=アプリ)に割り当てるスタック領域の内、ユーザモードで使用する領
    域のスタックのベースアドレス(一番値の大きいアドレス)である0x20001800を
    APP_STACK_BASE_UESRという名前で定数にする。
    
    併せて、特権モードで使用する領域のベースアドレスは、APP_STACK_BASE_KERN
    へ変更する。

--- kernel/task.c
+++ kernel/task.c
@@ -9,7 +9,8 @@
 
 #define GDT_IDX_OFS	5
 #define APP_ENTRY_POINT	0x20000020
-#define APP_STACK_BASE	0x20002000
+#define APP_STACK_BASE_USER	0x20001800
+#define APP_STACK_BASE_KERN	0x20002000
 #define GDT_USER_CS_OFS	0x0018
 #define GDT_USER_DS_OFS	0x0020
 
@@ -101,9 +102,9 @@ void task_init(struct file *f)
 
 	/* Setup task_tss */
 	new_task->tss.eip = APP_ENTRY_POINT;
-	new_task->tss.esp = 0x20001800;
+	new_task->tss.esp = APP_STACK_BASE_USER;
 	new_task->tss.eflags = 0x00000200;
-	new_task->tss.esp0 = APP_STACK_BASE;
+	new_task->tss.esp0 = APP_STACK_BASE_KERN;
 	new_task->tss.ss0 = GDT_KERN_DS_OFS;
 	new_task->tss.es = GDT_USER_DS_OFS | 0x0003;
 	new_task->tss.cs = GDT_USER_CS_OFS | 0x0003;
