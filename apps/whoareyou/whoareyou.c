98b9b668768b2aca72651576cc2544a11fd5cc3c
Yuma Ohgami <yuma@ohgami.jp>
2016-09-23 20:52:33 +0900

    app: whoareyou: add an example of using argc and argv
    argcとargvの使用例として、whoareyouコマンドを"-v"(verbose)オプションに対
    応させた。

--- apps/whoareyou/whoareyou.c
+++ apps/whoareyou/whoareyou.c
@@ -1,6 +1,30 @@
 #include <app.h>
 #include <kernel.h>
 
+static int str_compare(const char *src, const char *dst)
+{
+	char is_equal = 1;
+
+	for (; (*src != '\0') && (*dst != '\0'); src++, dst++) {
+		if (*src != *dst) {
+			is_equal = 0;
+			break;
+		}
+	}
+
+	if (is_equal) {
+		if (*src != '\0') {
+			return 1;
+		} else if (*dst != '\0') {
+			return -1;
+		} else {
+			return 0;
+		}
+	} else {
+		return (int)(*src - *dst);
+	}
+}
+
 unsigned int syscall(unsigned int syscall_id, unsigned int arg1, unsigned int arg2, unsigned int arg3)
 {
 	unsigned int result;
@@ -15,7 +39,10 @@ unsigned int syscall(unsigned int syscall_id, unsigned int arg1, unsigned int ar
 
 int main(int argc, char *argv[])
 {
-	syscall(SYSCALL_CON_PUT_STR, (unsigned int)"OS5\r\n", 0, 0);
+	if ((argc >= 2) && !str_compare(argv[1], "-v"))
+		syscall(SYSCALL_CON_PUT_STR, (unsigned int)"Operating System 5\r\n", 0, 0);
+	else
+		syscall(SYSCALL_CON_PUT_STR, (unsigned int)"OS5\r\n", 0, 0);
 	syscall(SYSCALL_EXIT, 0, 0, 0);
 
 	return 0;
