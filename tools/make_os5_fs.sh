006ecc57692d8054bbb59872320fa6eca3064405
Yuma Ohgami <yuma@ohgami.jp>
2016-09-27 23:40:39 +0900

    tool: fs: change indent to tab
    半角スペースのインデントをタブ文字へ変更する。

--- tools/make_os5_fs.sh
+++ tools/make_os5_fs.sh
@@ -6,14 +6,14 @@ dd if=/dev/zero count=4095 bs=1
 
 # make data block(s)
 while [ -n "$1" ]; do
-    # make header
-    name=$(basename $1)
-    echo -n $name
-    dd if=/dev/zero count=$((32 - ${#name})) bs=1
+	# make header
+	name=$(basename $1)
+	echo -n $name
+	dd if=/dev/zero count=$((32 - ${#name})) bs=1
 
-    # make data
-    dd if=$1
-    dd if=/dev/zero count=$((4096 - 32 - $(stat -c '%s' $1))) bs=1
+	# make data
+	dd if=$1
+	dd if=/dev/zero count=$((4096 - 32 - $(stat -c '%s' $1))) bs=1
 
-    shift
+	shift
 done
