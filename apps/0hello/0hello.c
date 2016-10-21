dfb71a295d26219b15b32813675f923c82f18e87
Yuma Ohgami <yuma@ohgami.jp>
2016-10-22 00:15:56 +0900

    add sample code

--- apps/0hello/0hello.c
+++ apps/0hello/0hello.c
@@ -9,6 +9,9 @@
 #define VRAM_SIZE	0x10000
 #define RES_WIDTH	320
 #define RES_HEIGHT	200
+#define START_BAR_HEIGHT	20
+#define START_BUTTON_WIDTH	30
+#define START_BUTTON_HEIGHT	13
 
 void fill_bg(unsigned char color)
 {
@@ -43,7 +46,18 @@ int main(int argc, char *argv[])
 
 	while (1) {
 		fill_bg(COLOR_BG);
-		fill_rect(0, RES_HEIGHT - 10, RES_WIDTH, 10, COLOR_FG);
+
+		/* start bar */
+		fill_rect(0, RES_HEIGHT - START_BAR_HEIGHT, RES_WIDTH, START_BAR_HEIGHT, COLOR_FG);
+		fill_rect(0, RES_HEIGHT - START_BAR_HEIGHT, RES_WIDTH, 1, 0x1e);
+		fill_rect(0, RES_HEIGHT - START_BAR_HEIGHT, 1, START_BAR_HEIGHT, 0x1e);
+
+		/* start button */
+		fill_rect(3, RES_HEIGHT - START_BAR_HEIGHT + 3 + START_BUTTON_HEIGHT, START_BUTTON_WIDTH + 1, 1, 0x13);
+		fill_rect(3 + START_BUTTON_WIDTH, RES_HEIGHT - START_BAR_HEIGHT + 3, 1, START_BUTTON_HEIGHT, 0x13);
+		fill_rect(3, RES_HEIGHT - START_BAR_HEIGHT + 3, START_BUTTON_WIDTH + 1, 1, 0x1e);
+		fill_rect(3, RES_HEIGHT - START_BAR_HEIGHT + 3, 1, START_BUTTON_HEIGHT, 0x1e);
+
 		get_line(buf, MAX_LINE_SIZE);
 	}
 
