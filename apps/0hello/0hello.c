a4950ff98c37beeb2819cb5adcf39f5469718fed
Yuma Ohgami <yuma@ohgami.jp>
2016-10-21 09:08:34 +0900

    add sample code
    画面描画のサンプル

--- apps/0hello/0hello.c
+++ apps/0hello/0hello.c
@@ -3,21 +3,48 @@
 #include <console.h>
 
 #define MAX_LINE_SIZE	512
+#define COLOR_BG	0x7d
+#define COLOR_FG	0x1c
+#define VRAM_BASE_ADDR	0x203f0000
+#define VRAM_SIZE	0x10000
+#define RES_WIDTH	320
+#define RES_HEIGHT	200
+
+void fill_bg(unsigned char color)
+{
+	unsigned int addr;
+	volatile unsigned char *p;
+
+	for (addr = VRAM_BASE_ADDR; addr < VRAM_BASE_ADDR + VRAM_SIZE; addr++) {
+		p = (volatile unsigned char *)addr;
+		*p = color;
+	}
+}
+
+void fill_rect(unsigned int x, unsigned int y, unsigned int w, unsigned int h, unsigned char color)
+{
+	unsigned int i, j;
+	unsigned int addr;
+	volatile unsigned char *p;
+
+	for (i = y; i < y + h; i++) {
+		for (j = x; j < x + w; j++) {
+			addr = VRAM_BASE_ADDR + (i * RES_WIDTH) + j;
+			p = (volatile unsigned char *)addr;
+			*p = color;
+		}
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	volatile unsigned char *p;
-	unsigned int addr;
 	unsigned char c = 0;
 	char buf[MAX_LINE_SIZE];
 
 	while (1) {
-		for (addr = 0x203f0000; addr < 0x20400000; addr++) {
-			p = (volatile unsigned char *)addr;
-			*p = c;
-		}
+		fill_bg(COLOR_BG);
+		fill_rect(0, RES_HEIGHT - 10, RES_WIDTH, 10, COLOR_FG);
 		get_line(buf, MAX_LINE_SIZE);
-		c++;
 	}
 
 	return 0;
