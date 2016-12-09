305a88582b6ccb029d21407a5d3def1c1bbdcc4b
Yuma Ohgami <yuma@ohgami.jp>
2016-12-10 07:04:12 +0900

    Revert "tool: fs: support the file size exceeds block size"
    カーネル側の対応ができていないため、
    8140e10b0dfcfe3bd97cd6655b7067479fab14e8 のコミットを一旦、revertする。

--- tools/make_os5_fs.sh
+++ tools/make_os5_fs.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-block_size=4096
-
 # make control block
 echo $# | awk '{printf "%c", $1}'
-dd if=/dev/zero count=$((block_size - 1)) bs=1
+dd if=/dev/zero count=4095 bs=1
 
 # make data block(s)
 while [ -n "$1" ]; do
@@ -12,20 +10,10 @@ while [ -n "$1" ]; do
 	name=$(basename $1)
 	echo -n $name
 	dd if=/dev/zero count=$((32 - ${#name})) bs=1
-	header_size=48
-	data_size=$(stat -c '%s' $1)
-	block_num=$((((header_size + data_size) / block_size) + 1))
-	echo $block_num | awk '{printf "%c", $1}'
-	dd if=/dev/zero count=15 bs=1
 
 	# make data
 	dd if=$1
-	data_size_1st_block=$((block_size - header_size))
-	if [ $data_size -le $data_size_1st_block ]; then
-		dd if=/dev/zero count=$((data_size_1st_block - data_size)) bs=1
-	else
-		dd if=/dev/zero count=$((block_size - ((header_size + data_size) % block_size))) bs=1
-	fi
+	dd if=/dev/zero count=$((4096 - 32 - $(stat -c '%s' $1))) bs=1
 
 	shift
 done
