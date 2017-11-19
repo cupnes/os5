d1d073a6c7256844d603fdbea7ef54d85473c9fe
Yuma Ohgami <yuma@ohgami.jp>
2017-11-19 20:34:40 +0900

    boot: uefi: add UEFI bootloader.
    UEFI対応のブートローダーを追加する。併せて、自作のUEFIライブラリ
    "libuefi"を追加する。
    
    現状は、カーネルとユーザーランドをRAMへロードするのみ。
    
    カーネルとユーザーランドはブートローダーのビルド時にobjcopyでバイナリ
    のオブジェクトファイルへ変換する。これにより、バイナリの開始アドレス・
    終了アドレス・サイズがシンボル情報としてオブジェクトファイルへ設定され
    る。ブートローダーはこれらのシンボル情報を利用して各バイナリのアドレス
    とサイズを知る。
    
    そして、ロード先を決めるため、UEFIのBootServicesのGetMemoryMap関数でメ
    モリマップを取得する(init_memmap関数)。取得したメモリマップから、コン
    ベンショナルメモリで、かつ、カーネルとユーザーランドに加えヒープとスタッ
    クを想定した1MBを確保できる領域を探し、最初に見つけた領域をロード先と
    する(get_allocatable_area関数)。(全部をまとめて一つの領域で確保しよう
    としているのは単に簡単のため)
    
    ロード後は、現状では、ロードしたカーネルとユーザーランドそれぞれの先頭
    8バイトを出力し、無限ループに入る。

--- Makefile
+++ Makefile
@@ -1,7 +1,15 @@
 all: fd.img
 
+fs/EFI/BOOT/BOOTX64.EFI: boot/uefi/image.efi
+	mkdir -p fs/EFI/BOOT
+	cp $< $@
+
 fd.img: boot/legacy_bios/boot.bin kernel/kernel.bin apps/apps.img
 	cat $+ > $@
+
+boot/uefi/image.efi: kernel/kernel.bin apps/apps.img
+	make -C boot/uefi KERNEL=../../kernel/kernel.bin \
+		APPS=../../apps/apps.img
 
 boot/legacy_bios/boot.bin:
 	make -C boot/legacy_bios
@@ -16,13 +24,20 @@ doc:
 	make -C doc
 
 clean:
+	make -C boot/uefi clean
 	make -C boot/legacy_bios clean
 	make -C kernel clean
 	make -C apps clean
 	make -C doc clean
-	rm -f *~ *.o *.bin *.dat *.img *.map
+	rm -rf *~ *.o *.bin *.dat *.img *.map fs
 
-run: fd.img
+run: run32
+
+run64: fs/EFI/BOOT/BOOTX64.EFI
+	qemu-system-x86_64 -bios OVMF.fd -hda fat:fs
+
+run32: fd.img
 	qemu-system-i386 -fda $<
 
-.PHONY: boot/legacy_bios/boot.bin kernel/kernel.bin apps/apps.img doc clean
+.PHONY: boot/legacy_bios/boot.bin boot/uefi/image.efi kernel/kernel.bin \
+	apps/apps.img doc clean run run64 run32
---
+++ boot/uefi/boot.c
@@ -0,0 +1,67 @@
+#include <efi.h>
+#include <common.h>
+#include <mem.h>
+
+#define STACK_HEAP_SIZE	1048576	/* 1MB */
+
+void efi_main(void *ImageHandle __attribute__ ((unused)),
+	      struct EFI_SYSTEM_TABLE *SystemTable)
+{
+	extern unsigned char _binary_kernel_bin_start;
+	extern unsigned char _binary_kernel_bin_size;
+	extern unsigned char _binary_apps_img_start;
+	extern unsigned char _binary_apps_img_size;
+
+	unsigned long long alloc_size;
+	struct EFI_MEMORY_DESCRIPTOR *mdesc;
+	unsigned long long alloc_addr;
+	void *kernel_start;
+	void *apps_start;
+	unsigned char *p;
+	unsigned int i;
+
+	efi_init(SystemTable);
+
+	puts(L"Starting OS5 UEFI bootloader ...\r\n");
+
+	alloc_size = (unsigned long long)&_binary_kernel_bin_size +
+		(unsigned long long)&_binary_apps_img_size + STACK_HEAP_SIZE;
+	puts(L"(kern img size) + (apps img size) + (stack and heap size) = 0x");
+	puth(alloc_size, 16);
+	puts(L"\r\n");
+
+	init_memmap();
+
+	mdesc = get_allocatable_area(alloc_size);
+	alloc_addr = mdesc->PhysicalStart;
+	puts(L"alloc addr = 0x");
+	puth(alloc_addr, 16);
+	puts(L"\r\n");
+
+	kernel_start = (void *)alloc_addr;
+	ST->BootServices->CopyMem(kernel_start,
+				  (void *)&_binary_kernel_bin_start,
+				  (unsigned long long)&_binary_kernel_bin_size);
+	alloc_addr += (unsigned long long)&_binary_kernel_bin_size;
+	apps_start = (void *)alloc_addr;
+	ST->BootServices->CopyMem(apps_start,
+				  (void *)&_binary_apps_img_start,
+				  (unsigned long long)&_binary_apps_img_size);
+
+	puts(L"loaded kernel(first 8 bytes): ");
+	p = (unsigned char *)kernel_start;
+	for (i = 0; i < 8; i++) {
+		puth(*p++, 2);
+		putc(L' ');
+	}
+	puts(L"\r\n");
+	puts(L"loaded apps(first 8 bytes): ");
+	p = (unsigned char *)apps_start;
+	for (i = 0; i < 8; i++) {
+		puth(*p++, 2);
+		putc(L' ');
+	}
+	puts(L"\r\n");
+
+	while (TRUE);
+}
