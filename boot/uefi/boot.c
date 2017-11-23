6eaf1e8135f4b14287bba4628b895ddcbca2ee4e
Yuma Ohgami <yuma@ohgami.jp>
2017-11-23 14:56:38 +0900

    boot: uefi: skip loading apps.img if loading it fails.
    app.imgのロードに失敗した場合はスキップするよう機能を追加する。

--- boot/uefi/boot.c
+++ boot/uefi/boot.c
@@ -26,6 +26,7 @@ void efi_main(void *ImageHandle, struct EFI_SYSTEM_TABLE *SystemTable)
 	unsigned int i;
 	unsigned long long kernel_arg1;
 	unsigned long long kernel_arg2;
+	unsigned char has_apps = TRUE;
 
 	efi_init(SystemTable);
 
@@ -42,12 +43,22 @@ void efi_main(void *ImageHandle, struct EFI_SYSTEM_TABLE *SystemTable)
 
 	status = root->Open(
 		root, &file_apps, APPS_FILE_NAME, EFI_FILE_MODE_READ, 0);
-	assert(status, L"root->Open(apps)");
+	if (!check_warn_error(status, L"root->Open(apps)")) {
+		puts(L"apps load failure. skip.\r\n");
+		has_apps = FALSE;
+	}
 
-	apps_size = get_file_size(file_apps);
+	if (has_apps)
+		apps_size = get_file_size(file_apps);
 
-	alloc_size = kernel_size + apps_size + STACK_HEAP_SIZE;
-	puts(L"(kern bin size) + (apps img size) + (stack and heap size) = 0x");
+	alloc_size = kernel_size + STACK_HEAP_SIZE;
+	if (has_apps) {
+		puts(L"(kern bin size) + (apps img size) + ");
+		puts(L"(stack and heap size) = 0x");
+		alloc_size += apps_size;
+	} else
+		puts(L"(kern bin size) + (stack and heap size) = 0x");
+
 	puth(alloc_size, 16);
 	puts(L"\r\n");
 
@@ -66,10 +77,13 @@ void efi_main(void *ImageHandle, struct EFI_SYSTEM_TABLE *SystemTable)
 	kernel_start = (void *)alloc_addr;
 	status = file_kernel->Read(file_kernel, &kernel_size, kernel_start);
 	assert(status, L"file_kernel->Read");
-	alloc_addr += kernel_size;
-	apps_start = (void *)alloc_addr;
-	status = file_apps->Read(file_apps, &apps_size, apps_start);
-	assert(status, L"file_apps->Read");
+
+	if (has_apps) {
+		alloc_addr += kernel_size;
+		apps_start = (void *)alloc_addr;
+		status = file_apps->Read(file_apps, &apps_size, apps_start);
+		assert(status, L"file_apps->Read");
+	}
 
 	puts(L"loaded kernel(first 8 bytes): ");
 	p = (unsigned char *)kernel_start;
@@ -78,16 +92,19 @@ void efi_main(void *ImageHandle, struct EFI_SYSTEM_TABLE *SystemTable)
 		putc(L' ');
 	}
 	puts(L"\r\n");
-	puts(L"loaded apps(first 8 bytes): ");
-	p = (unsigned char *)apps_start;
-	for (i = 0; i < 8; i++) {
-		puth(*p++, 2);
-		putc(L' ');
+	if (has_apps) {
+		puts(L"loaded apps(first 8 bytes): ");
+		p = (unsigned char *)apps_start;
+		for (i = 0; i < 8; i++) {
+			puth(*p++, 2);
+			putc(L' ');
+		}
+		puts(L"\r\n");
 	}
-	puts(L"\r\n");
 
 	file_kernel->Close(file_kernel);
-	file_apps->Close(file_apps);
+	if (has_apps)
+		file_apps->Close(file_apps);
 
 	kernel_arg1 = (unsigned long long)ST;
 	init_fb();
